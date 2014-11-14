8d05c8326598fdcf00de82e00d371346abfd6a0c
Sheena Artrip <sheena.artrip@onlinebankingsolutions.com>
2014-11-14 15:04:23 -0500

    Added experiment1 and install scripts for the services

--- util/install.sh
+++ util/install.sh
@@ -13,3 +13,21 @@ systemctl enable fleetd
 systemctl start etcd
 systemctl start fleetd
 systemctl start docker
+
+fleetctl load /opt/docker/src/services/experiment1.service
+
+fleetctl load /opt/docker/src/services/main/vulcand.service
+
+fleetctl load /opt/docker/src/services/data/apache.1.data.service
+fleetctl load /opt/docker/src/services/data/apache.2.data.service
+fleetctl load /opt/docker/src/services/data/apache.3.data.service
+
+fleetctl load /opt/docker/src/services/main/apache.1.service
+fleetctl load /opt/docker/src/services/main/apache.2.service
+fleetctl load /opt/docker/src/services/main/apache.3.service
+
+fleetctl load /opt/docker/src/services/sidekicks/apache.1.sk.service
+fleetctl load /opt/docker/src/services/sidekicks/apache.2.sk.service
+fleetctl load /opt/docker/src/services/sidekicks/apache.3.sk.service
+
+fleetctl start experiment1
