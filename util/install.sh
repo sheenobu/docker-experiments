36e15816a19896d51ed574fbf0fba40c9a438442
Sheena Artrip <sheena.artrip@onlinebankingsolutions.com>
2014-11-14 20:21:18 -0500

    Working multi-node apache server in front of vulcand

--- util/install.sh
+++ util/install.sh
@@ -1,4 +1,5 @@
 
+PATH="$PATH:/opt/docker/bin"
 
 cp -f /opt/docker/src/base/docker.service /usr/lib/systemd/system/docker.service
 cp -f /opt/docker/src/base/etcd.service /usr/lib/systemd/system/etcd.service
@@ -14,13 +15,26 @@ systemctl start etcd
 systemctl start fleetd
 systemctl start docker
 
-fleetctl load /opt/docker/src/services/experiment1.service
-
+fleetctl unload vulcand.service
+fleetctl destroy vulcand.service
 fleetctl load /opt/docker/src/services/main/vulcand.service
+fleetctl start vulcand.service
 
-fleetctl load /opt/docker/src/services/data/apache.1.data.service
-fleetctl load /opt/docker/src/services/data/apache.2.data.service
-fleetctl load /opt/docker/src/services/data/apache.3.data.service
+fleetctl unload apache.endpoint.service
+fleetctl unload apache.1.service
+fleetctl unload apache.1.sk.service
+fleetctl unload apache.2.service
+fleetctl unload apache.2.sk.service
+fleetctl unload apache.3.service
+fleetctl unload apache.3.sk.service
+
+fleetctl destroy apache.endpoint.service
+fleetctl destroy apache.1.service
+fleetctl destroy apache.1.sk.service
+fleetctl destroy apache.2.service
+fleetctl destroy apache.2.sk.service
+fleetctl destroy apache.3.service
+fleetctl destroy apache.3.sk.service
 
 fleetctl load /opt/docker/src/services/main/apache.1.service
 fleetctl load /opt/docker/src/services/main/apache.2.service
@@ -30,4 +44,13 @@ fleetctl load /opt/docker/src/services/sidekicks/apache.1.sk.service
 fleetctl load /opt/docker/src/services/sidekicks/apache.2.sk.service
 fleetctl load /opt/docker/src/services/sidekicks/apache.3.sk.service
 
-fleetctl start experiment1
+fleetctl load /opt/docker/src/services/main/apache.endpoint.service
+
+fleetctl start apache.1.service
+fleetctl start apache.2.service
+fleetctl start apache.3.service
+
+fleetctl start apache.endpoint.service
+fleetctl start apache.1.sk.service
+fleetctl start apache.2.sk.service
+fleetctl start apache.3.sk.service
